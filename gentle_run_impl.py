18af7a3ea21f191868b36e08084b3b94954c39bd
kdavis-mozilla <kdavis@mozilla.com>
2018-08-30 17:41:55 +0200

    Added initial alignment script

--- gentle_run_impl.py
+++ gentle_run_impl.py
@@ -1,60 +1,53 @@
-import argparse
+import os
+import glob
+import pathlib
 import logging
 import multiprocessing
-import os
-import sys
+import xml.etree.ElementTree as ET
 
 import gentle
 
-parser = argparse.ArgumentParser(
-        description='Align a transcript to audio by generating a new language model.  Outputs JSON')
-parser.add_argument(
-        '--nthreads', default=multiprocessing.cpu_count(), type=int,
-        help='number of alignment threads')
-parser.add_argument(
-        '-o', '--output', metavar='output', type=str, 
-        help='output filename')
-parser.add_argument(
-        '--conservative', dest='conservative', action='store_true',
-        help='conservative alignment')
-parser.set_defaults(conservative=False)
-parser.add_argument(
-        '--disfluency', dest='disfluency', action='store_true',
-        help='include disfluencies (uh, um) in alignment')
-parser.set_defaults(disfluency=False)
-parser.add_argument(
-        '--log', default="INFO",
-        help='the log level (DEBUG, INFO, WARNING, ERROR, or CRITICAL)')
-parser.add_argument(
-        'audiofile', type=str,
-        help='audio file')
-parser.add_argument(
-        'txtfile', type=str,
-        help='transcript text file')
-args = parser.parse_args()
-
-log_level = args.log.upper()
-logging.getLogger().setLevel(log_level)
-
 disfluencies = set(['uh', 'um'])
+
+nthreads = multiprocessing.cpu_count()
+
+source_glob_suffix = '**/*.xml'
+source_glob_prefix = '/snakepit/shared/data/NPR/WAMU'
+
+target_prefix = '/snakepit/uploads/NPR/WAMU'
+
+namespaces = {'vx' : 'http://www.voxant.com/NewsML/transcript'}
+
+glob_path = os.path.join(source_glob_prefix, source_glob_suffix)
+
+resources = gentle.Resources()
 
 def on_progress(p):
     for k,v in p.items():
-        logging.debug("%s: %s" % (k, v))
+        print("%s: %s" % (k, v))
 
-
-with open(args.txtfile) as fh:
-    transcript = fh.read()
-
-resources = gentle.Resources()
-logging.info("converting audio to 8K sampled wav")
-
-with gentle.resampled(args.audiofile) as wavfile:
-    logging.info("starting alignment")
-    aligner = gentle.ForcedAligner(resources, transcript, nthreads=args.nthreads, disfluency=args.disfluency, conservative=args.conservative, disfluencies=disfluencies)
-    result = aligner.transcribe(wavfile, progress_cb=on_progress, logging=logging)
-
-fh = open(args.output, 'w') if args.output else sys.stdout
-fh.write(result.to_json(indent=2))
-if args.output:
-    logging.info("output written to %s" % (args.output))
+for source_path_xml in glob.glob(glob_path, recursive=True):
+    target_path_txt = os.path.join(target_prefix, os.path.relpath(source_path_xml, source_glob_prefix)).replace('.xml', '.txt')
+    pathlib.Path(os.path.dirname(target_path_txt)).mkdir(parents=True, exist_ok=True)
+    with open(target_path_txt, 'w+') as target_file_txt:
+        parsed_transcript = ET.parse(source_path_xml)
+        parsed_root = parsed_transcript.getroot()
+        for turn in parsed_root.findall('.//vx:Turn', namespaces):
+            if 'DISCLAIMER' != turn.attrib['Speaker']:
+                for fragment in turn.findall('.//vx:Fragment', namespaces):
+                    target_file_txt.write(fragment.text)
+ 
+    with open(target_path_txt) as target_file_txt:
+        transcript = target_file_txt.read()
+ 
+    source_path_mp3 = source_path_xml.replace('transcripts/extracted', 'audio').replace('.xml', '.mp3')
+    if os.path.isfile(source_path_mp3):
+        target_path_json = target_path_txt.replace('.txt', '.json')
+        with open(target_path_json, 'w') as target_file_json:
+            print('converting audio to 8K sampled wav')
+            with gentle.resampled(source_path_mp3) as wavfile:
+                print('starting alignment for', source_path_xml, ' and ', source_path_mp3)
+                aligner = gentle.ForcedAligner(resources, transcript, nthreads=nthreads, disfluency=False, conservative=False, disfluencies=disfluencies)
+                result = aligner.transcribe(wavfile, progress_cb=on_progress, logging=logging)
+                target_file_json.write(result.to_json(indent=2))
+        print('finished alignment in', target_path_json)
